1a2314ee4d8b69f0d8479449b5def7f45f1e9468
JioSky <jiosky.buyback@gmail.com>
2023-07-12 21:48:28 +0300

    Добавил Header

---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+<head>
+   <?php require_once "templates/head.php" ?>
+</head>
+<body>
+<?php require_once "templates/header.php" ?>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,7 @@
+<header class="navbar navbar-light bg-light p-2">
+    <a class="navbar-brand">Cloth Shop</a>
+    <form class="form-inline">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sing in</button>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sing up</button>
+    </form>
+</header>
